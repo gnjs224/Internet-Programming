2524bb44cc853d5ee7bff31c304f2e255b74b4e3
gnjs224 <32116171+gnjs224@users.noreply.github.com>
2021-05-20 23:51:25 +0900

    5/20

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
+# 5/12 주제 정하기 및 디자인
+
 # 주제: 나의 개인 홈페이지.
 
 # imforamtion, record, project, goal, plan, diary
+
+# 5/13 initial commit
+
+# 5/17 네비게이션바, 홈화면 구현
+
+# 5/18 다이어리화면 중 달력 구현
+
+# 5/20 서버 구현
+
+# 참고자료: https://hyun-am-coding.tistory.com/entry/REST-API-%EC%98%88%EC%A0%9C, https://neosla.tistory.com/20
--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 var http = require("http");
 var express = require("express");
-var fs = require("fs");
 var app = express();
-var router = express.Router();
 app.use(express.static("public"));
 app.get("", function (req, res) {
   res.sendFile(__dirname + "/home.html");
@@ -14,6 +12,7 @@ app.get("/me", function (req, res) {
 app.get("/main", function (req, res) {
   res.sendFile(__dirname + "/main.png");
 });
+
 http.createServer(app).listen(52273, function () {
   console.log("Server Running at http://127.0.0.1:52273");
 });
